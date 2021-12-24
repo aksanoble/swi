5f96d786157d27be45e228e8f7b5219cd194d5f0
Akshay Kanthi <aksanoble@gmail.com>
2021-12-24 15:37:20 +0530

    tailwind-ui + application shell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios(
+          "https://data.covid19india.org/v4/min/data.min.json"
+        );
+
+        setData(result.data);
+      } catch (error) {
+        alert("Something went wrong!");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <Home />;
 }
 
 export default App;
